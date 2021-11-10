ff4aa8e1b1ce4251e2fbe78e31b7faeb9569e7a9
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-11-10 16:23:54 -0500

    commit tree 3d4a9c1f0a42b4f6bf94dea4e38abb246816e7ba

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        # binding.pry
+        self.actors.map do |i|
+            i.full_name
+        end
+    end
+
 end
